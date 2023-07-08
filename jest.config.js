fe5629a8810bf969dd2e8a840a8723eb5624b706
samirm00 <sammoham.be@gmail.com>
2023-07-08 15:55:36 +0200

    initial commit

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,10 @@
 export default {
-  coverageThreshold: {
-    global: {
-      // branches: 100, // so code with no branches can still pass
-      functions: 100,
-      lines: 100,
-      statements: 100,
+    coverageThreshold: {
+        global: {
+            // branches: 100, // so code with no branches can still pass
+            functions: 100,
+            lines: 100,
+            statements: 100,
+        },
     },
-  },
 };
---
+++ template-solution/solution.js
@@ -0,0 +1,6 @@
+/**
+ *
+ */
+export const solutionName = (num1, num2) => {
+    return num1 + num2;
+};
