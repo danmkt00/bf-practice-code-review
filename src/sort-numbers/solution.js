3b87a088a6927e4e9f7bf73e2dd3b0bd85ad0e62
samirm00 <sammoham.be@gmail.com>
2023-07-09 09:11:41 +0200

    initial commit

--- src/sort-numbers/solution.js
+++ src/sort-numbers/solution.js
@@ -1,5 +1,9 @@
-/**
+/** .........
+ * Sum of two numbers
  *
+ * @param {number} num1 - the first number to sum
+ * @param {number} num2 - the second number to sum
+ * @returns {number} the sum of num1 and num2
  */
 export const solutionName = (num1, num2) => {
     return num1 + num2;
